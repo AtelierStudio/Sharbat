3fa20687fd23f575b515d1a1ab52f62f096fa07f
Junseok Oh <kotohana5706@edcan.kr>
2016-07-16 16:38:58 +0900

    Reconfigure Layout

--- app/src/main/java/kr/edcan/sharbat/activity/MailViewActivity.java
+++ app/src/main/java/kr/edcan/sharbat/activity/MailViewActivity.java
@@ -3,6 +3,8 @@ package kr.edcan.sharbat.activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import kr.edcan.sharbat.R;
+
 public class MailViewActivity extends AppCompatActivity {
 
     @Override
